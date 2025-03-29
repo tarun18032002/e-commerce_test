b72e76fbe2b135c911a54322160b835898ea4c0c
tarunkumar18032002 <tarunkumar18032002@gmail.com>
2025-03-30 00:38:07 +0530

    django setup and home page done

--- password.py
+++ password.py
@@ -1,2 +1,5 @@
-git config --global user.name "tarunkumar182002"
+git config --global user.name "tarunkumar18032002"
 git config --global user.email "tarunkumar18032002@gmail.com"
+
+
+git credential reject https://github.com/basapathitarun
